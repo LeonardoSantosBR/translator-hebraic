3515ce465f05f9e6d6c5649c4017ca0aaab09472
leonardoSantos-dell <leosantosmelo07@gmail.com>
2025-05-15 20:53:11 -0300

    feat: lado direito

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import Header from "./components/header";
+import { Input } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="w-full min-h-screen flex flex-col">
       <Header />
-      <div className="w-full flex-[9] bg-[#dfe3ee]"></div>
+
+      <div className="w-full flex-[9] bg-[#dfe3ee] grid grid-cols-2">
+        <div className="w-full flex justify-center items-center bg-gray-200">
+          <div className="w-3/4 max-w-md bg-blue-500 text-white p-6 rounded-xl shadow-lg text-center">
+            <Input
+              placeholder="Digite aqui..."
+              size="lg"
+              variant="flushed"
+              color="white"
+              className="placeholder:text-white text-white"
+            />
+          </div>
+        </div>
+
+        <div>
+          <p className="text-lg font-medium">Lado Direito</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,8 @@ import { FaBookBookmark } from "react-icons/fa6";
 
 function Header() {
   return (
-    <div className="w-full flex-[1] bg-[#3b5998] flex items-center">
-      <div className="flex items-center gap-4 p-2">
+    <div className="w-full flex-[1] bg-[#3b5998] flex justify-start items-center">
+      <div className="flex ml-3 gap-4">
         <h1 className="text-2xl font-bold text-gray-100">Hebrai</h1>
         <FaBookBookmark color="#44bec7" size={30} />
       </div>
