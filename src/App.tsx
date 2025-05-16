4a170cddbba312c5d5856ff0e5020aaeb71292a0
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-05-16 16:46:21 -0300

    feat: ajustes de estados para guardar texto

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import { Input } from "@chakra-ui/react";
 
 function App() {
   const [text, setText] = useState("");
+  const [result, setResult] = useState("");
 
-  useEffect(() => {});
+  useEffect(() => {}, [text]);
 
   return (
     <div className="w-full min-h-screen flex flex-col">
       <Header />
-
       <div className="w-full flex-[9] bg-[#dfe3ee] grid grid-cols-2">
         <div className="w-full flex justify-center items-center bg-gray-200">
           <div className="w-3/4 max-w-md bg-blue-500 text-white p-6 rounded-xl shadow-lg text-center">
@@ -25,7 +25,6 @@ function App() {
             />
           </div>
         </div>
-
         <div className="bg-slate-300">
           <p className="text-lg font-medium p-5 text-black underline">
             Lado Direito
---
+++ src/http/post-translate.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export async function postTranslate({ text }: { text: string }) {
+  const request = await axios.post(
+    `https://translation.googleapis.com/language/translate/v2?key=${import.meta.env.VITE_API_KEY}`,
+    {
+      q: text,
+      source: "pt",
+      target: "he",
+      format: "text",
+    }
+  );
+
+  const data = await request.data;
+  return data;
+}
