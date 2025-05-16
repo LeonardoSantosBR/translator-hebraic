6d37b511e448f3744391c3945a2dc5a1439ebad8
leonardoSantos-dell <leosantosmelo07@gmail.com>
2025-05-16 20:00:16 -0300

    feat: spinner correto

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -1,9 +1,9 @@
-import { TailSpin } from "react-loader-spinner";
+import { ThreeDots } from "react-loader-spinner";
 
 function Spinner() {
   return (
-    <div className="w-full h-full fixed bg-slate-400 bg-opacity-55 flex justify-center items-center">
-      <TailSpin height="80" width="80" color="blue" ariaLabel="loading" />
+    <div className="h-full bg-slate-400 bg-opacity-55 flex justify-center items-center">
+      <ThreeDots height="80" width="80" color="blue" ariaLabel="loading" />
     </div>
   );
 }
